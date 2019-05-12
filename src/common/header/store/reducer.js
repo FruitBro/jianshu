bc9dec2269885b77ee4564110dea72282fdef434
FriutBro <2285042109@qq.com>
2019-05-12 21:10:42 +0800

    优化代码

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -9,16 +9,14 @@ const defaultState = fromJS({
 // 手动的保证state不被修改存在风险，引入immutable，生成immutable对象，不可变，state.set对数据进行变更，实际是返回了全新的state
 // 把不同的了类型进行分类
 export default (state = defaultState, action) => {
-    if (action.type === constants.SEARCH_FOCUS) {
-        // immutable对象的set方法，会结合之前immutable对象的值
-        // 和设置的值，返回一个全新的对象
-        return state.set('focused', true)
+    switch (action.type) {
+        case constants.SEARCH_FOCUS:
+          return state.set('focused', true)
+        case constants.SEARCH_BLUR:
+          return state.set('focused', false)
+        case constants.CHANGE_LIST:
+          return state.set('list', action.data)
+        default:
+            return state
     }
-    if (action.type === constants.SEARCH_BLUR) {
-        return state.set('focused', false)
-    }
-    if (action.type === constants.CHANGE_LIST) {
-        return state.set('list', action.data)
-    }
-    return state
 }
